e6d69227819d6443f0b28ddac2552a9ae5a3bb0c
Christopher Dent <christopherdent01@gmail.com>
2019-08-04 02:17:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         }
      }
 
-  string = programmer_hash[:dennis_ritchie][:languages]
+  string = programmer_hash[:dennis_ritchie][:languages].to_s
     string.chomp
 
 end
